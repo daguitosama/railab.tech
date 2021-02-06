d0a9306dbad49662db8de594140070da8ff46fa6
d4g0 <tocarralero@gmail.com>
2021-02-06 14:41:31 -0500

    baseline

---
+++ components/FormSample.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="max-w-screen-lg w-4/5 xl:w-full mx-auto antialiased flex flex-col items-center"
+  >
+    <h1 class="font-extrabold text-6xl pt-20">Focus Visible Sample</h1>
+
+    <h2 class="text-bold text-xl mt-12 max-w-lg md:text-center">
+      This submit button just get a focus ring when focused by keyboard
+      interations (TAB) .
+    </h2>
+    <form class="mt-10 w-full max-w-sm" @submit.prevent="">
+      <fieldset>
+        <label for="petName" class="block ml-4">Pet Name</label>
+        <input
+          id="petName"
+          type="text"
+          placeholder="type your pet name here"
+          class="block mt-2 px-4 py-2 w-full rounded-lg bg-gray-700 shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-600"
+        />
+      </fieldset>
+
+      <div class="mt-6 w-full">
+        <button
+          type="submit"
+          class="py-2 w-full px-4 rounded-lg bg-indigo-700 hover:bg-indigo-800 shadow-lg focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-400"
+        >
+          Get your Free Food
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
