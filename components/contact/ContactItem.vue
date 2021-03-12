294606968a2f1faceeef48d12407505afd62bf49
d4g0 <tocarralero@gmail.com>
2021-03-12 17:57:45 -0500

    focus-visible ring

--- components/contact/ContactItem.vue
+++ components/contact/ContactItem.vue
@@ -2,7 +2,7 @@
   <a
     :href="contactItem.link"
     :title="contactItem.tooltip"
-    class="shadow bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-900 rounded-2xl py-4 px-16 flex justify-center items-center"
+    class="shadow bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-900 rounded-2xl py-4 px-16 flex justify-center items-center focus:outline-none focus-visible:ring focus-visible:ring-primary-light dark:focus-visible:ring-primary-dark "
   >
     <slot></slot>
     <p class="ml-2">
