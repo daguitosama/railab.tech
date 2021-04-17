c82d22eb44abd2d401b213967314a6a17f87f192
d4g0 <tocarralero@gmail.com>
2021-04-17 05:42:25 -0400

    about fix

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="min-h-screen">
-    <div class="container flex flex-col items-center">
-      <AppHeading :sectionHeading="$t('about.heading')" />
-      <div class="mt-6 space-y-4 max-w-xs opacity-80">
-        <p class="">{{ $t("about.description.p1") }}</p>
-        <p class="">{{ $t("about.description.p2") }}</p>
-        <p class="">{{ $t("about.description.p3") }}</p>
+  <div class="min-h-84 pb-20 pt-10">
+    <div class="container flex justify-center items-center">
+      <div class="max-w-lg">
+        <AppHeading :sectionHeading="$t('about.heading')" />
+        <div class="mt-6 space-y-4  opacity-80">
+          <p class="">{{ $t("about.description.p1") }}</p>
+          <p class="">{{ $t("about.description.p2") }}</p>
+          <p class="">{{ $t("about.description.p3") }}</p>
+        </div>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
@@ -34,13 +36,12 @@ export default {
       title: this.$t("about.head.title"),
       titleTemplate: "%s | Railab",
       meta: [
-
         {
           hid: "description",
           name: "description",
           content: this.$t("about.head.description"),
         },
-        
+
         // og
         {
           hid: "og:title",
