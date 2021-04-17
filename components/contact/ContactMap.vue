06ae458fd7f8d6be8b45aa7806489cf29778270a
d4g0 <tocarralero@gmail.com>
2021-04-17 05:02:10 -0400

    map iframe [-] btn a11y fix

--- components/contact/ContactMap.vue
+++ components/contact/ContactMap.vue
@@ -5,10 +5,12 @@
       class="text-center"
     />
     <div class="relative mt-10 h-80 md:h-96">
-      <Map/>
-      <div class="w-full absolute bottom-0 z-10 flex justify-center">
+      <Map />
+      <div
+        class="w-full absolute bottom-0 z-10 flex justify-center h-0 bg-gray-300"
+      >
         <div
-          class="max-w-15 md:max-w-sm py-3 px-4 md:px-8 md:py-6 -mb-16 sm:-mb-12 shadow-lg text-center rounded-2xl bg-white dark:bg-gray-800 text-onLight dark:text-onDark"
+          class="max-w-15 absolute md:max-w-sm px-2 py-4 -top-4 md:-top-12 shadow-lg text-center rounded-2xl bg-white dark:bg-gray-800 text-onLight dark:text-onDark"
         >
           <address>{{ $t("contact.address") }}</address>
           <p class="mt-2">{{ $t("contact.scheduleTimeStr") }}</p>
@@ -22,11 +24,11 @@
 import { reactive, ref, toRefs, watch } from "@vue/composition-api";
 import { useElementVisibility } from "@vueuse/core";
 import AppHeading from "@/components/app/AppHeading.vue";
-import Map from '@/components/contact/Map.vue'
+import Map from "@/components/contact/Map.vue";
 export default {
   components: {
     AppHeading,
-    Map
+    Map,
   },
   props: {
     heading: {
