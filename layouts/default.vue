816f3b63c935209d9ca1c5cdcadb0f4879ef8fb3
d4g0 <tocarralero@gmail.com>
2021-04-18 18:13:56 -0400

    lazy load app notifications

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div
     class="bg-surface-light text-onLight dark:bg-surface-dark dark:text-onDark transition-colors duration-300 ease-linear"
   >
-    <SkipToMainLinkn/>
+    <SkipToMainLinkn />
     <AppNavigation />
     <AppError />
     <AppNotification />
@@ -16,17 +16,14 @@
 
 <script>
 import AppNavigation from "@/components/app/AppNavigation.vue";
-import AppFooter from "@/components/app/AppFooter.vue";
-import AppError from "@/components/app/AppError.vue";
-import AppNotification from "@/components/app/AppNotification.vue";
-import SkipToMainLinkn from '@/components/app/SkipToMainLink.vue'
+import SkipToMainLinkn from "@/components/app/SkipToMainLink.vue";
 export default {
   components: {
     AppNavigation,
-    AppFooter,
-    AppError,
-    AppNotification,
-    SkipToMainLinkn
+    AppFooter: () => import("@/components/app/AppFooter.vue"),
+    AppError: () => import("@/components/app/AppError.vue"),
+    AppNotification: () => import("@/components/app/AppNotification.vue"),
+    SkipToMainLinkn,
   },
   head() {
     return Object.assign({}, this.$nuxtI18nSeo());
