3c939adcb63ed30cf04b79f3083f6f33c9a882f0
d4g0 <tocarralero@gmail.com>
2021-06-23 17:23:11 -0400

    abr

---
+++ pages/table.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="
+      min-h-screen
+      w-full
+      bg-surface-light
+      dark:bg-surface-dark
+      text-onLight
+      dark:text-onDark
+      flex flex-col
+      items-center
+      justify-center
+      gap-6
+    "
+  >
+    <button class="bg-gray-200 h-32" style="height: 40px; width: 60px">
+      btn
+    </button>
+    <div class="bg-gray-200 w-[40px] h-[40px] ">div</div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "empty",
+};
+</script>
+
+<style>
+</style>
